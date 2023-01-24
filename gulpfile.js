81214837af8875f4e6342bb94c964f967fddb571
Wojciech.Otawski <wojciech.otawski@gmail.com>
2023-01-24 21:06:01 +0100

    Added autoprefixer for css

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,16 @@
 const { src, dest, watch, series } = require("gulp");
-const { exec } = require("child_process");
 const sass = require("gulp-sass")(require("sass"));
 const cleanCss = require("gulp-clean-css");
 const terser = require("gulp-terser");
 var concat = require('gulp-concat');
 const browsersync = require("browser-sync").create();
+const autoprefixer = require('gulp-autoprefixer');
 
 function scssTask() {
   return src("src/scss/*.scss")
     .pipe(sass())
     .pipe(dest("src/css/single/"))
+    .pipe(autoprefixer())
     .pipe(concat(`style.css`))
     .pipe(cleanCss())
     .pipe(dest("src/css"));
